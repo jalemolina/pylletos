2ffc3ced91e5a91b83b0e442727f03b4b69b668c
Alejandro Molina <jalemolina@gmail.com>
2017-11-18 22:15:51 -0300

    preparando el terreno

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pylletos',
+      version='0.2.1',
+      description='Creador de folletos a partir de archivos .pdf',
+      author='J. Alejandro Molina',
+      author_email='jalemolina@gmail.com',
+      packages=['pylletos'])
+
+__author__ = 'J. Alejandro Molina'
